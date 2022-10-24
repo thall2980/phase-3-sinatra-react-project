8d8106a030e2607e9a5c8d6f87963004bcdd8db4
Briana <briana_brewster4@yahoo.com>
2022-10-24 10:15:59 -0500

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@ Walker.create(name: "Steve", years_of_experience: 3, pace: fast)
 Walker.create(name: "Rachel", years_of_experience: 1, pace: slow)
 Walker.create(name: "Caroline", years_of_experience: 5, pace: medium)
 
+puts "🌱 Seeding dogs..."
+
 Dog.create(name: "Rex", owner: "Bill", breed: "Lab", temperament: "friendly", age: 2)
 Dog.create(name: "Pugsly", owner: "Jess", breed: "Pug", temperament: "lazy", age: 4)
 Dog.create(name: "Lassie", owner: "Frank", breed: "Golden Retriever", temperament: "friendly", age: 4)
@@ -12,6 +14,8 @@ Dog.create(name: "Skip", owner: "Lauren", breed: "Corgi", temperament: "energeti
 Dog.create(name: "Lady", owner: "Jen", breed: "Poodle", temperament: "sassy", age: 6)
 Dog.create(name: "Gidget", owner: "Charlie" , breed: "Chihuahua", temperament: "friendly", age: 4)
 Dog.create(name: "Chase", owner: "Tyler", breed: "Blue Nose Pitbull", temperament: "energetic", age: 2)
+
+puts "🌱 Seeding appointments..."
 
 Appointment.create(date: "10-26-2022", dog_id:rand(1..8), walker_id:rand(1..3))
 Appointment.create(date: "10-28-2022", dog_id:rand(1..8), walker_id:rand(1..3))
