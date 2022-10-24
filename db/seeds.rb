ac1563a480131cd4b0b4e539b670b7f59b2fc569
Tyler Hall <thall2980@gmail.com>
2022-10-24 11:14:53 -0400

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,14 @@ Walker.create(name: "Steve", years_of_experience: 3, pace: fast)
 Walker.create(name: "Rachel", years_of_experience: 1, pace: slow)
 Walker.create(name: "Caroline", years_of_experience: 5, pace: medium)
 
-puts "🌱 Seeding appointments..."
+Dog.create(name: "Rex", owner: "Bill", breed: "Lab", temperament: "friendly", age: 2)
+Dog.create(name: "Pugsly", owner: "Jess", breed: "Pug", temperament: "lazy", age: 4)
+Dog.create(name: "Lassie", owner: "Frank", breed: "Golden Retriever", temperament: "friendly", age: 4)
+Dog.create(name: "Fido", owner: "Patrick", breed: "Dalmation", temperament: "energetic", age: 5)
+Dog.create(name: "Skip", owner: "Lauren", breed: "Corgi", temperament: "energetic", age: 3)
+Dog.create(name: "Lady", owner: "Jen", breed: "Poodle", temperament: "sassy", age: 6)
+Dog.create(name: "Gidget", owner: "Charlie" , breed: "Chihuahua", temperament: "friendly", age: 4)
+Dog.create(name: "Chase", owner: "Tyler", breed: "Blue Nose Pitbull", temperament: "energetic", age: 2)
 
 Appointment.create(date: "10-26-2022", dog_id:rand(1..8), walker_id:rand(1..3))
 Appointment.create(date: "10-28-2022", dog_id:rand(1..8), walker_id:rand(1..3))
